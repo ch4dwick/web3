8154170868049eba5f0faebb1520f5258b8ed259
Omar Morelos <>
2022-06-06 12:30:26 +0800

    Initial implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Web3 from "web3";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      web3: new Web3("ws://127.0.0.1:7545"),
+      accounts: null,
+    };
+  }
+
+  componentDidMount() {
+    this.getAccounts();
+  }
+
+  getAccounts() {
+    this.state.web3.eth
+      .getAccounts()
+      .then((res) => {
+        this.setState({
+          accounts: res,
+        });
+      })
+      .catch(console.log);
+  }
+
+  render() {
+    const accounts = this.state.accounts;
+    var addresses;
+    if (accounts) {
+      addresses = accounts.map((account, key) => {
+        return <li key={key}>{account}</li>;
+      });
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          Accounts:
+          <ul className="App-body">{addresses}</ul>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
